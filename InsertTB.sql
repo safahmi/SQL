3a8e252b538b39034cedc75b9c29447724990a14
safahmi <97179134+safahmi@users.noreply.github.com>
2022-02-03 10:49:45 +0100

    insert tests part

--- InsertTB.sql
+++ InsertTB.sql
@@ -17,4 +17,14 @@ INSERT INTO `student` (`name`, `city`) VALUES ('Aube', 'Bordeaux');
 INSERT INTO `matter` (`codemat`, `coef`) VALUES ('sciences', '1');
 INSERT INTO `matter` (`codemat`, `coef`) VALUES ('maths', '2');
 INSERT INTO `matter` (`codemat`, `coef`) VALUES ('physics', '3');
+/*
+* 5 tests are required, 3 of which have the date between 1/1/2014 and 30/6/2014 (inclusive).
+* You also need 2 subjects with more than one test.
+*/
+INSERT INTO `test` (`testdate`, `codemat`) VALUES ('2014-01-05', 'sciences');
+INSERT INTO `test` (`testdate`, `codemat`) VALUES ('2014-01-10', 'physics');
+INSERT INTO `test` (`testdate`, `codemat`) VALUES ('2014-01-20', 'sciences');
+INSERT INTO `test` (`testdate`, `codemat`) VALUES ('2015-02-01', 'physics');
+INSERT INTO `test` (`testdate`, `codemat`) VALUES ('2015-03-02', 'maths');
+
 
