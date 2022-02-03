94a26b43a4ed51bb5bf9179df3a52d122f91de31
safahmi <97179134+safahmi@users.noreply.github.com>
2022-02-03 10:31:57 +0100

    Insert student part

---
+++ InsertTB.sql
@@ -0,0 +1,8 @@
+USE notationStudentsDB;
+INSERT INTO `student` (`name`, `city`) VALUES ('Dupont', 'Marseille');
+INSERT INTO `student` (`name`, `city`) VALUES ('Durant', 'Paris');
+INSERT INTO `student` (`name`, `city`) VALUES ('Martin', 'Séville');
+INSERT INTO `student` (`name`, `city`) VALUES ('Fahmi', 'Goussainville');
+INSERT INTO `student` (`name`, `city`) VALUES ('Reda', 'Paris');
+INSERT INTO `student` (`name`, `city`) VALUES ('Hammer', 'Paris');
+INSERT INTO `student` (`name`, `city`) VALUES ('Aube', 'Bordeaux');
